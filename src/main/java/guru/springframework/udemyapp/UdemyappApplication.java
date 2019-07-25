78dd5215d25cdd4bf2a38cf48ec1f16df312e1b7
Radek <roszko.radek@gmail.com>
2019-07-25 22:21:02 +0200

    init

---
+++ src/main/java/guru/springframework/udemyapp/UdemyappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.udemyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/udemyapp/UdemyappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.udemyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
